f9abf4cacb2362eb433f19169da987806f15fe13
William Wang <bwilliamwang@gmail.com>
2024-11-16 23:36:52 +0700

    working

--- app/api/verify/route.ts
+++ app/api/verify/route.ts
@@ -1,41 +1,41 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   try {
     const proof = await request.json();
-    console.log('Received proof:', proof);
-    
-    // Your verification logic here
-    
-    return NextResponse.json({ message: 'Verification successful' });
+    console.log("Received proof:", proof);
+
+    const app_id = "app_4020275d788fc6f5664d986dd931e5e6";
+    const action = "verify";
+    // const response = await fetch(
+    //   'https://developer.worldcoin.org/api/v2/verify/app_4020275d788fc6f5664d986dd931e5e6',
+    //   {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({ ...proof, action: "verify"}),
+    //   }
+    // );
+    // console.log(response)
+    // if (response.ok) {
+    //   const { verified } = await response.json();
+    //   return verified;
+    // } else {
+    //   const { code, detail } = await response.json();
+    //   throw new Error(`Error Code ${code}: ${detail}`);
+    // }
+    // const verifyRes = (await verifyCloudProof(
+    //   proof,
+    //   app_id,
+    //   action
+    // )) as IVerifyResponse;
+    // console.log(verifyRes)
+
+
+    return NextResponse.json({ message: "Verification successful" });
   } catch (error) {
-    console.error('Error processing verification:', error);
-    return NextResponse.json({ error: 'Verification failed' }, { status: 400 });
+    console.error("Error processing verification:", error);
+    return NextResponse.json({ error: "Verification failed" }, { status: 400 });
   }
 }
-// import { type IVerifyResponse, verifyCloudProof } from "@worldcoin/idkit";
-// import { NextRequest, NextResponse } from 'next/server';
-
-// export default async function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse
-// ) {
-//   const proof = req.body;
-//   const app_id = "app_4020275d788fc6f5664d986dd931e5e6";
-//   const action = "verify";
-//   const verifyRes = (await verifyCloudProof(
-//     proof,
-//     app_id,
-//     action
-//   )) as IVerifyResponse;
-
-//   if (verifyRes.success) {
-//     // This is where you should perform backend actions if the verification succeeds
-//     // Such as, setting a user as "verified" in a database
-//     res.status(200).send(verifyRes);
-//   } else {
-//     // This is where you should handle errors from the World ID /verify endpoint.
-//     // Usually these errors are due to a user having already verified.
-//     res.status(400).send(verifyRes);
-//   }
-// }
