53832bb05dec2eb78b6351b1d6c007c852674e42
William Wang <bwilliamwang@gmail.com>
2024-11-16 14:39:04 +0700

    home good

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,14 @@
 
 import React from "react";
 // import Image from "next/image";
-import { useDynamicContext } from "@dynamic-labs/sdk-react-core";
 
-import HandleSubmit from "./HandleSubmit";
-import {SplashPage} from "@/components/SplashPage";
+import { SplashPage } from "@/components/SplashPage";
 
 export default function Home() {
-  const { primaryWallet } = useDynamicContext();
-  const publicKey = primaryWallet?.address;
 
   return (
     <div className="container p-4 max-w-md align-middle justify-center">
-      {!publicKey && <SplashPage /> }
-
-      {publicKey && <HandleSubmit />}
+      <SplashPage />
     </div>
   );
 }
--- components/ButtomNav.tsx
+++ components/ButtomNav.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { Camera, Book, User } from 'lucide-react'
+import { User, Home, Map } from 'lucide-react'
 
 const BottomNav = () => {
   const pathname = usePathname()
@@ -10,11 +10,11 @@ const BottomNav = () => {
   return (
     <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 flex justify-around items-center h-16 animate-slide-up">
       <Link href="/" className={`flex flex-col items-center ${pathname === '/' ? 'text-blue-500' : 'text-gray-500'}`}>
-        <Camera className="w-6 h-6" />
-        <span className="text-xs">Add Record</span>
+        <Home className="w-6 h-6" />
+        <span className="text-xs">Home</span>
       </Link>
       <Link href="/gallery" className={`flex flex-col items-center ${pathname === '/gallery' ? 'text-blue-500' : 'text-gray-500'}`}>
-        <Book className="w-6 h-6" />
+        <Map className="w-6 h-6" />
         <span className="text-xs">Map</span>
       </Link>
       <Link href="/profile" className={`flex flex-col items-center ${pathname === '/profile' ? 'text-blue-500' : 'text-gray-500'}`}>
